bd4bcb6a89ca9df74ebd89a6fee915c19cafce2d
Dominic <Domdombossy@gmail.com>
2024-09-25 16:02:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    //class monthAsker
+    //main()
+    //num birthMonth
+    //output "Please enter your birth month [1-12]"
+    //input birthMonth
+    //if birthMonth >= 1 and birthMonth <= 12  then
+    //output “Your birth month is:” + birthMonth
+    //else
+    // 		output “Error, you entered an invalid month. You entered:” + birthMonth
+    //end if
+    //return
+    //end class
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+    }
+}
